94c7e52e3b03e52f9195fc2f5fb11746599df213
Katz9891 <trabajosklg@gmail.com>
2025-03-17 22:20:15 -0500

    cambios nuevos

--- Funciones/FuncionRegular2.py
+++ Funciones/FuncionRegular2.py
@@ -1,2 +1,10 @@
-def invertiordenclaves():
-    
+def invertiordenclaves(clave):
+    z=0 #inicializamos z en 0 para almacenar el numero invertido
+    x=clave
+
+    while x>0:
+        k=x%10 #obtiene el ultimo digito de x
+        z=z*10+k #agrega el digito a z
+        x=x//10 #elimina el ultimo digito de x
+
+    print (z)
